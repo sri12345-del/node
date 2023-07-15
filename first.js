441e6e68fae644b5a130541226fac8b53084eb88
sri12345-del <129243474+sri12345-del@users.noreply.github.com>
2023-07-15 13:30:52 +0530

    task2

--- first.js
+++ first.js
@@ -1,1 +1,10 @@
-console.log('hello world')
+const print = (a, b) => a * b
+console.log(print(3, 4))
+
+const obj = {
+    name: 'srini',
+    greet: function(){
+        console.log(this.name)
+    }
+}
+obj.greet()
