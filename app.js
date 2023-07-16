c366328a501a47ef5dfb507c366c742ba3cb70f6
sri12345-del <129243474+sri12345-del@users.noreply.github.com>
2023-07-16 12:37:29 +0530

    task7

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('srinivas');
+    let url = req.url
+    if (url === "/home") {
+        res.setHeader('Content-Type','text/html')
+        res.write('<html>')
+        res.write('<head><title>my first page></title></head>')
+        res.write('<body><header>welcome to the home</header></body>')
+        res.write('</html>')
+        return res.end() 
+    } else if (url === '/about') {
+        res.write('<html>')
+    res.write('<head><title>my first page></title></head>')
+        res.write('<body><header>welcome to the about</header></body>')
+        res.write('</html>')
+    return res.end()
+    } else if (url === '/node') {
+        res.write('<html>')
+        res.write('<head><title>my first page></title></head>')
+            res.write('<body><header>welcome to the node</header></body>')
+            res.write('</html>')
+        return res.end()
+    }
+
+    
 });
 
-server.listen(3002);
+server.listen(3005);
